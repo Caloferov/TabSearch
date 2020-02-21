696182128d2fa2ece3befd172a23078d6df3ea94
Cyril Caloferov <32363923+Caloferov@users.noreply.github.com>
2020-02-21 09:50:32 +0200

    Add files via upload

---
+++ server/mockData.js
@@ -0,0 +1,338 @@
+module.exports = {
+    mock: {
+        "kind": "youtube#searchListResponse",
+        "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/-413LYyhB-G39tNIxRWUd5rKfJs\"",
+        "nextPageToken": "CAoQAA",
+        "regionCode": "BG",
+        "pageInfo": {
+            "totalResults": 1000000,
+            "resultsPerPage": 10
+        },
+        "items": [
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/9OkOFoEMrABIRdxOJnKQWQAV8B0\"",
+                "id": {
+                    "kind": "youtube#channel",
+                    "channelId": "UCWJ2lWNubArHWmf3FIHbfcQ"
+                },
+                "snippet": {
+                    "publishedAt": "2005-11-21T01:20:33.000Z",
+                    "channelId": "UCWJ2lWNubArHWmf3FIHbfcQ",
+                    "title": "NBA",
+                    "description": "National Basketball Association. Official home of the most compelling basketball action from the NBA.",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://yt3.ggpht.com/-iykIxE6HFpM/AAAAAAAAAAI/AAAAAAAAAAA/qho7mLdsPLc/s88-c-k-no-mo-rj-c0xffffff/photo.jpg"
+                        },
+                        "medium": {
+                            "url": "https://yt3.ggpht.com/-iykIxE6HFpM/AAAAAAAAAAI/AAAAAAAAAAA/qho7mLdsPLc/s240-c-k-no-mo-rj-c0xffffff/photo.jpg"
+                        },
+                        "high": {
+                            "url": "https://yt3.ggpht.com/-iykIxE6HFpM/AAAAAAAAAAI/AAAAAAAAAAA/qho7mLdsPLc/s800-c-k-no-mo-rj-c0xffffff/photo.jpg"
+                        }
+                    },
+                    "channelTitle": "NBA",
+                    "liveBroadcastContent": "upcoming"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/X7YNcv8sU18FHym3QcGGECk4UYU\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "xPYDdZbuPzI"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-18T22:31:16.000Z",
+                    "channelId": "UCWJ2lWNubArHWmf3FIHbfcQ",
+                    "title": "Every NBA All-Star Game MVP in League History | Kobe, LeBron, Kawhi, and More!",
+                    "description": "With Kawhi Leonard winning the 2020 NBA All-Star Game Kobe Bryant MVP Award, we take a look back at every All-Star MVP award winner in league history!",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/xPYDdZbuPzI/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/xPYDdZbuPzI/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/xPYDdZbuPzI/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "NBA",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/v_ZNjTUOhQE0TT_uJR0MbxcdpUo\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "Z-KdfIwnqds"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-17T07:12:02.000Z",
+                    "channelId": "UCWJ2lWNubArHWmf3FIHbfcQ",
+                    "title": "Best Plays From 2020 NBA All-Star Game!",
+                    "description": "Check out the BEST PLAYS from the 2020 NBA All-Star Game! Subscribe to the NBA: https://on.nba.com/2JX5gSN Full Game Highlights Playlist: ...",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/Z-KdfIwnqds/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/Z-KdfIwnqds/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/Z-KdfIwnqds/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "NBA",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/XNyAY0P73JpeAldVYd2kr2RD5w0\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "5yAIynThyVU"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-17T08:35:45.000Z",
+                    "channelId": "UCWJ2lWNubArHWmf3FIHbfcQ",
+                    "title": "NBA Top 10 Plays of the Night | February 16, 2020",
+                    "description": "Check out the top 10 plays of the night from around the league on Feb. 16 featuring LeBron James, Devin Booker, Kemba Walker and more! Subscribe to the ...",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/5yAIynThyVU/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/5yAIynThyVU/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/5yAIynThyVU/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "NBA",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/tMG3FrwTtqNz89zatMep2Sb-DDI\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "eSXBikj6d0U"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-17T04:42:42.000Z",
+                    "channelId": "UCqQo7ewe87aYAe7ub5UqXMw",
+                    "title": "2020 NBA All-Star Game - Full Game Highlights - Team LeBron vs Team Giannis - February 16, 2020",
+                    "description": "2020 NBA All-Star Game - Full Game Highlights - Team LeBron vs Team Giannis - February 16, 2020 REGISTER FOR BRxCHI FEATURING QUAVO ON FEB ...",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/eSXBikj6d0U/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/eSXBikj6d0U/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/eSXBikj6d0U/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "House of Highlights",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/6Yb2UvIghZ5Oh5mPFOo-s0y7ylg\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "BsL7pjxko7Q"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-16T05:43:46.000Z",
+                    "channelId": "UC9-OpMMVoNP5o10_Iyq7Ndw",
+                    "title": "2020 NBA Dunk Contest Full Highlights",
+                    "description": "Every dunk and highlight from the 2020 NBA Dunk Contest between Derrick Jones Jr., Aaron Gordon, Dwight Howard and Pat Connaughton. Subscribe: ...",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/BsL7pjxko7Q/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/BsL7pjxko7Q/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/BsL7pjxko7Q/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "Bleacher Report",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/AyGvChL86Nc4NvexMyCVODHGyf4\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "XNXLIWIeFKE"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-17T21:30:00.000Z",
+                    "channelId": "UCUCfpsXkgfx9rdkYXNAgDSQ",
+                    "title": "NBA&#39;s Most Embarrassing Plays",
+                    "description": "Get SportzCases Here! - https://SportzCases.com/?aff=299 Promo code for 10% off - BRAZY DISCLAIMER : All clips are property of the NBA. No copyright ...",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/XNXLIWIeFKE/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/XNXLIWIeFKE/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/XNXLIWIeFKE/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "BraZy",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/_GFs7CTAyIkI6Ru5u71jN2-DXsY\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "RzFcef8-_RQ"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-17T21:00:04.000Z",
+                    "channelId": "UCiWLfSweyRNmLpgEHekhoAg",
+                    "title": "Reacting to the INTENSE 2020 NBA All-Star Game: Well, that worked! | The Jump",
+                    "description": "Rachel Nichols recaps and reacts to the intense 2020 NBA All-Star Game, which featured Kobe Bryant-inspired format changes and an intense battle between ...",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/RzFcef8-_RQ/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/RzFcef8-_RQ/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/RzFcef8-_RQ/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "ESPN",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/PZdCPJv2bqGUgHvkLQTtVjSsxsA\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "c8PZ_EGowPo"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-16T02:29:37.000Z",
+                    "channelId": "UCqQo7ewe87aYAe7ub5UqXMw",
+                    "title": "2020 NBA Three-Point Contest - Championship Round - Full Highlights",
+                    "description": "2020 NBA Three-Point Contest - Full Highlights - 2020 NBA All-Star Saturday Night REGISTER FOR BRxCHI FEATURING QUAVO ON FEB 16!",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/c8PZ_EGowPo/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/c8PZ_EGowPo/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/c8PZ_EGowPo/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "House of Highlights",
+                    "liveBroadcastContent": "none"
+                }
+            },
+            {
+                "kind": "youtube#searchResult",
+                "etag": "\"Fznwjl6JEQdo1MGvHOGaz_YanRU/XWj0F5ScKwpCFnBJZg-RmeQreII\"",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "6r6FACZq898"
+                },
+                "snippet": {
+                    "publishedAt": "2020-02-17T22:20:43.000Z",
+                    "channelId": "UCeFlXzfdGnMWSqd_6vzXmEA",
+                    "title": "NBA &quot;Too Much Sauce&quot; MOMENTS",
+                    "description": "NBA \"Too Much Sauce\" Moments showcase some of the greatest ankle breakers, dribble moves and jelly layups throughout the 2019-2020 NBA Season.",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/6r6FACZq898/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/6r6FACZq898/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/6r6FACZq898/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "KingSwish",
+                    "liveBroadcastContent": "none"
+                }
+            }
+        ]
+    },
+  };
---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const mockData = require("./mockData")
+
+const mock = mockData.mock;
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const baseUrl = "https://www.googleapis.com/youtube/v3/search?"
+
+const apiKey = "AIzaSyAuKFvOehdUIapTUkje_9zKNHVdGEQ1RVk";
+app.get('/search', (req, res) => {
+    let maxResults = req.query.maxResults ? req.query.maxResults : '5';
+    let type = req.query.type;
+    let q = req.query.q;
+    let finalURL;
+    if (req.query.pageToken) {
+        finalURL = baseUrl + "pageToken=" + req.query.pageToken + "&";
+    } else {
+        finalURL = baseUrl;
+    }
+
+    let data = '';
+    // Contact the youtube API
+    https.get(`${finalURL}part=snippet&maxResults=${maxResults}&type=${type}&q=${q}&key=${apiKey}`, (resp) => {
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        resp.on('end', () => {
+            res.send(JSON.parse(data));
+            // Send mocked data
+            // res.send(mock)
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        res.send(err.message)
+    });
+})
+
+app.listen(3000);
